4bd7fce94a4b4018035be9044dbdc75130b35f36
durandnico <durandnico@cy-tech.fr>
2025-01-01 20:49:44 +0100

    which-key + lsp + copilot

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -4,7 +4,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({",	",	
+    vim.api.nvim_echo({",	",
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
       { "\nPress any key to exit..." },
@@ -41,21 +41,85 @@ require("lazy").setup({
 		  end
 	  },
 
-	 -- colorized code keyword
+	  -- colorized code keyword
 	  {
-		  "nvim-treesitter/nvim-treesitter", 
+		  "nvim-treesitter/nvim-treesitter",
 		  build = ":TSUpdate"
 	  },
 
 	  -- mason : external package manager such as lsp/linter
 	  {
 		  "williamboman/mason.nvim",
-		  "williamboman/mason-lspconfig.nvim"
+		  lazy =false,
+		  config = function()
+			  require("mason").setup()
+		  end
+	  },
+
+	  {
+		  "williamboman/mason-lspconfig.nvim",
+		  lazy =false,
+		  config = function()
+			  require("mason-lspconfig").setup({
+				  ensure_installed = {
+					-- "lua_ls",
+
+				  }
+			  })
+		  end,
 	  },
 
 	  -- lsp config
 	  {
 		  "neovim/nvim-lspconfig",
+	  },
+
+	  -- zero-lsp
+	  -- TODO : setup this thing (/after/plugins/lsp)
+	  {
+		  'VonHeikemen/lsp-zero.nvim',
+		  branch = 'v4.x',
+		  dependencies = {
+			  -- LSP Support
+			  {'neovim/nvim-lspconfig'},             -- Required
+
+			  -- Autocompletion
+			  {'hrsh7th/nvim-cmp'},         -- Required
+			  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+			  {'hrsh7th/cmp-buffer'},       -- Optional
+			  {'hrsh7th/cmp-path'},         -- Optional
+			  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+			  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+			  -- Snippets
+			  {'L3MON4D3/LuaSnip'},             -- Optional
+			  {'rafamadriz/friendly-snippets'}, -- Optional
+		  },
+	  },
+
+	  -- which-key
+	  {
+		  "folke/which-key.nvim",
+		  event = "VeryLazy",
+		  opts = {
+			  -- your configuration comes here
+			  -- or leave it empty to use the default settings
+			  -- refer to the configuration section below
+		  },
+		  keys = {
+			  {
+				  "<leader>?",
+				  function()
+					  require("which-key").show({ global = false })
+				  end,
+				  desc = "Buffer Local Keymaps (which-key)",
+			  },
+		  },
+	  },
+
+	  -- copilot
+	  {
+		"github/copilot.vim",
 	  },
 
   },
