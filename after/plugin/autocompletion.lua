14e1881221d42ede19ef92bd34090a31a1df4c38
durandnico <durandnico@cy-tech.fr>
2025-01-01 23:52:51 +0100

    removing tab selection

--- after/plugin/autocompletion.lua
+++ after/plugin/autocompletion.lua
@@ -25,24 +25,7 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     }),
-
-
-    -- Simple tab complete
-    ['<Tab>'] = cmp.mapping(function(fallback)
-	    local col = vim.fn.col('.') - 1
-
-	    if cmp.visible() then
-		    cmp.select_next_item({behavior = 'select'})
-	    elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-		    fallback()
-	    else
-		    cmp.complete()
-	    end
-    end, {'i', 's'}),
-
-    -- Go to previous item
-    ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-  },
+ },
 
   snippet = {
     expand = function(args)
