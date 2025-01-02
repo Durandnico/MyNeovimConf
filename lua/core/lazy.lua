d8b4dc9c2b07cc70ffb8f1bcbd27d21526b80f66
durandnico <durandnico@cy-tech.fr>
2025-01-02 01:33:28 +0100

    format

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -4,9 +4,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({",	",
+    vim.api.nvim_echo({ ",	",
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -25,102 +25,105 @@ require("lazy").setup({
   spec = {
 
 
-	  -- fuzzy finder file
-	  {
-		  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		  -- or                              , branch = '0.1.x',
-		  dependencies = { 'nvim-lua/plenary.nvim' }
-	  },
+    -- fuzzy finder file
+    {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.8',
+      -- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
 
-	  -- color theme
-	  {
-		  "catppuccin/nvim", name = "catppuccin", priority = 1000,
-		  config = function()
-			  vim.cmd("colorscheme catppuccin-frappe")
-		  end
-	  },
+    -- color theme
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      priority = 1000,
+      config = function()
+        vim.cmd("colorscheme catppuccin-frappe")
+      end
+    },
 
-	  -- colorized code keyword
-	  {
-		  "nvim-treesitter/nvim-treesitter",
-		  build = ":TSUpdate"
-	  },
+    -- colorized code keyword
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate"
+    },
 
-	  -- mason : external package manager such as lsp/linter
-	  {
-		  "williamboman/mason.nvim",
-		  lazy =false,
-		  config = function()
-			  require("mason").setup()
-		  end
-	  },
+    -- mason : external package manager such as lsp/linter
+    {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+        require("mason").setup()
+      end
+    },
 
-	  {
-		  "williamboman/mason-lspconfig.nvim",
-		  lazy =false,
-		  config = function()
-			  require("mason-lspconfig").setup({
-				  ensure_installed = {
-					-- "lua_ls",
+    {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      config = function()
+        require("mason-lspconfig").setup({
+          ensure_installed = {
+            -- "lua_ls",
 
-				  }
-			  })
-		  end,
-	  },
+          }
+        })
+      end,
+    },
 
-	  -- lsp config
-	  {
-		  "neovim/nvim-lspconfig",
-	  },
+    -- lsp config
+    {
+      "neovim/nvim-lspconfig",
+    },
 
-	  -- zero-lsp
-	  -- TODO : setup this thing (/after/plugins/lsp)
-	  {
-		  'VonHeikemen/lsp-zero.nvim',
-		  branch = 'v4.x',
-		  dependencies = {
-			  -- LSP Support
-			  {'neovim/nvim-lspconfig'},             -- Required
+    -- zero-lsp
+    -- TODO : setup this thing (/after/plugins/lsp)
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v4.x',
+      dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' }, -- Required
 
-			  -- Autocompletion
-			  {'hrsh7th/nvim-cmp'},         -- Required
-			  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-			  {'hrsh7th/cmp-buffer'},       -- Optional
-			  {'hrsh7th/cmp-path'},         -- Optional
-			  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-			  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },         -- Required
+        { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+        { 'hrsh7th/cmp-buffer' },       -- Optional
+        { 'hrsh7th/cmp-path' },         -- Optional
+        { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+        { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-			  -- Snippets
-			  {'L3MON4D3/LuaSnip'},             -- Optional
-			  {'rafamadriz/friendly-snippets'}, -- Optional
-		  },
-	  },
+        -- Snippets
+        { 'L3MON4D3/LuaSnip' },             -- Optional
+        { 'rafamadriz/friendly-snippets' }, -- Optional
+      },
+    },
 
-	  -- which-key
-	  {
-		  "folke/which-key.nvim",
-		  event = "VeryLazy",
-		  opts = {
-			  -- your configuration comes here
-			  -- or leave it empty to use the default settings
-			  -- refer to the configuration section below
-		  },
-		  keys = {
-			  {
-				  "<leader>?",
-				  function()
-					  require("which-key").show({ global = false })
-				  end,
-				  desc = "Buffer Local Keymaps (which-key)",
-			  },
-		  },
-	  },
+    -- which-key
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    },
 
-	  -- copilot
-	  {
-		"github/copilot.vim",
-	  },
+    -- copilot
+    {
+      "github/copilot.vim",
+    },
 
   },
   -- Configure any other settings here. See the documentation for more details.
