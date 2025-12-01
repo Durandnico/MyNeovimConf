eb3e2a252f47d6ac17fce877846cb6a2ff0ace35
Nicolas Durand <nicod4956@gmail.com>
2025-12-01 23:34:54 +0100

    removing unused plugin

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -53,23 +53,15 @@ require("lazy").setup({
     -- mason : external package manager such as lsp/linter
     {
       "williamboman/mason.nvim",
-      lazy = false,
-      config = function()
-        require("mason").setup()
-      end
+      -- lazy = false,
+      -- config = function()
+      --   require("mason").setup()
+      -- end
     },
 
     {
       "williamboman/mason-lspconfig.nvim",
-      lazy = false,
-      config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            -- "lua_ls",
-
-          }
-        })
-      end,
+      -- lazy = false,
     },
 
     -- lsp config
@@ -100,6 +92,7 @@ require("lazy").setup({
       },
     },
 
+
     -- which-key
     {
       "folke/which-key.nvim",
@@ -124,94 +117,6 @@ require("lazy").setup({
     {
       "github/copilot.vim",
     },
-
-    -- dap
-    {
-      "mfussenegger/nvim-dap",
-      event = "VeryLazy",
-    },
-    {
-      "rcarriga/nvim-dap-ui",
-      dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
-    },
-
-    -- rust
-    {
-      "rust-lang/rust.vim",
-      ft = "rust",
-      config = function()
-        vim.g.rustfmt_autosave = 1
-        -- vim.g.rustfmt_fail_silently = 1
-        -- vim.g.rust_fold = 1
-        -- vim.g.rust_clip_command = "xclip -selection clipboard"
-      end,
-    },
-
-    {
-      "simrat39/rust-tools.nvim",
-      ft = "rust",
-      dependencies = { "neovim/nvim-lspconfig" },
-    },
-
-    -- avante.nvim, my maybe futur vibe code goto in life
-    -- {
-    --   "yetone/avante.nvim",
-    --   event = "VeryLazy",
-    --   version = false, -- Never set this value to "*"! Never!
-    --   opts = {
-    --     -- add any opts here
-    --     -- for example
-    --     provider = "copilot",
-    --     copilot = {
-    --       model = "claude-3.7-sonnet", -- your desired model (or use gpt-4o, etc.)
-    --       timeout = 60000, -- Timeout in milliseconds, increase this for reasoning models
-    --       temperature = 0,
-    --       max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-    --       reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-    --     },
-    --   },
-    --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    --   build = "make",
-    --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    --   dependencies = {
-    --     "nvim-treesitter/nvim-treesitter",
-    --     "stevearc/dressing.nvim",
-    --     "nvim-lua/plenary.nvim",
-    --     "MunifTanjim/nui.nvim",
-    --     --- The below dependencies are optional,
-    --     "echasnovski/mini.pick",     -- for file_selector provider mini.pick
-    --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    --     "hrsh7th/nvim-cmp",          -- autocompletion for avante commands and mentions
-    --     "ibhagwan/fzf-lua",          -- for file_selector provider fzf
-    --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    --     "zbirenbaum/copilot.lua",    -- for providers='copilot'
-    --     {
-    --       -- support for image pasting
-    --       "HakonHarnes/img-clip.nvim",
-    --       event = "VeryLazy",
-    --       opts = {
-    --         -- recommended settings
-    --         default = {
-    --           embed_image_as_base64 = false,
-    --           prompt_for_file_name = false,
-    --           drag_and_drop = {
-    --             insert_mode = true,
-    --           },
-    --           -- required for Windows users
-    --           use_absolute_path = true,
-    --         },
-    --       },
-    --     },
-    --     {
-    --       -- Make sure to set this up properly if you have lazy=true
-    --       'MeanderingProgrammer/render-markdown.nvim',
-    --       opts = {
-    --         file_types = { "markdown", "Avante" },
-    --       },
-    --       ft = { "markdown", "Avante" },
-    --     },
-    --   },
-    -- }
 
   },
   -- Configure any other settings here. See the documentation for more details.
